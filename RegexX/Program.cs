56f8efce2bd189838fc34084e5e3a74cb835a94e
Prasanth Louis <plouis@nextgen.com>
2016-06-06 16:38:58 -0400

    Next Commit

---
+++ RegexX/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pattern = args[0];
+            var matter = args[1];
+            var regex = new Regex(pattern);
+            var match = regex.Match(matter);
+            while (match.Success)
+            {
+                Console.WriteLine($"Match is found at {match.Index} and is {match.Length} long");
+                match = match.NextMatch();
+            }
+            Console.ReadLine();
+        }
+    }
+}
